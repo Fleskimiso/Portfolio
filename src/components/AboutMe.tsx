7bf695412848581f601d88bb19c1466fa61ea887
Fleskimiso <dawid82228@o2.pl>
2023-02-20 19:49:09 +0100

    added about me short description

--- src/components/AboutMe.tsx
+++ src/components/AboutMe.tsx
@@ -1,26 +1,59 @@
 const AboutMe = (props: { lang: "pl" | "en" }) => {
     return <div className="container-fluid vh-100 bg-gradient-aqua d-flex flex-column justify-content-between align-items-center" id='about-me'>
         <div className="left-white-traingle"></div>
-        <div className="">
+        <div className="w-auto">
             <div className="fs-2 text-white text-center">
                 {props.lang === "pl" ? "O mnie" : "About me"}
             </div>
-            <div className="text-white text-center fs-5 mx-auto col-10 col-sm-8 col-md-6 col-lg-4 ">
-                "TO DO"
-                Lorem ipsum, dolor sit amet consectetur adipisicing elit. Suscipit magni assumenda ipsam dolor corrupti ut quod reprehenderit exercitationem, soluta maxime qui quasi ex provident et. Necessitatibus cupiditate explicabo itaque culpa!
-                Corrupti similique delectus, labore eaque hic iure odit. Tenetur aperiam adipisci dolore quia obcaecati commodi minima officia laboriosam doloribus? Corrupti pariatur commodi neque soluta. Illum perspiciatis laboriosam illo alias aut!
-                Iure porro asperiores dolore totam culpa voluptates, quam harum ipsa nobis aut. Dolor, facere perferendis ipsam, unde fugit quibusdam nisi corrupti maiores quasi obcaecati officiis soluta ipsa laborum quis nostrum?
-                <div className="d-flex justify-content-left mt-3">
-                <button className="btn customBorder me-3">
-                    {/* to do add cv */}
-                    <a target="_blank" className="text-decoration-none text-white" href="#">CV</a>
-                </button>
-                <button className="btn customBorder ">
-                <a href="https://www.github.com/Fleskimiso" target="_blank" className="text-white text-decoration-none" >Github</a>
-                </button>
+            <div className="text-white text-left fs-5 mx-auto col-10 col-sm-8 col-md-6 col-lg-4 ">
+
+                {props.lang === "pl" ?
+                    <div style={{ "textIndent": "10%" }}>
+                        <p>
+                            Cześć, mam na imię Dawid Pawłowicz mam 20 lat i jestem web developerem. Jestem na początku swojej kariery programistycznej
+                            i szukam okazji i możliwości do rozwoju swoich umiejętności.
+                        </p>
+                        <p>
+                            Największe doświadczenie posiadam w stacku technologicznym
+                            javascript (MERN), jednak mam też mniejsze doświadczenie innych językach jako chociażby Python, C++, Java.
+                        </p>
+                        <p>
+                            Swoją przygodę z programowaniem zacząłem w roku 2018 od nauki C++, jednak od paru miesięcy wziąłem się poważniej za rozwój
+                            i uznałem, że techonologie internetowe to dziedzina, która najbardziej mnie interesuje.
+                        </p>
+                        <div style={{ "textIndent": "0%" }}>
+                            W wolnym czasie uwielbiam uprawiać sport.
+                        </div>
+                    </div> : <div>
+                        <p>
+                            Hello, my name is Dawid Pawłowicz, I am 20 years old and I am a web developer. I started my career as a programmer
+                            and I am looking for a job and opportunities to develop my skills.</p>
+                        <p>
+                            The biggest experience I have
+                            is in the stack javascript (MERN), but I have also other languages as Python, C, Java.
+                        </p>
+                        <p>
+                            My first programming experience started in 2018 learning C++,
+                            but I took my a bigger take on my development since last few months
+                            and decided that internet technologies are what is the most interesting to me.
+                        </p>
+                        <div style={{ "textIndent": "0%" }}>
+                            I like to exercise in free time.
+                        </div>
+                    </div>
+                }
+
+                <div className="d-flex flex-row justify-content-center mt-3">
+                    <button className="btn customBorder me-3">
+                        {/* to do add cv */}
+                        <a target="_blank" className="text-decoration-none text-white" href="#">CV</a>
+                    </button>
+                    <button className="btn customBorder ">
+                        <a href="https://www.github.com/Fleskimiso" target="_blank" className="text-white text-decoration-none" >Github</a>
+                    </button>
+                </div>
             </div>
-            </div>
-            
+
         </div>
         <div className="white-traingle"></div>
     </div>
--- src/components/Contact.tsx
+++ src/components/Contact.tsx
@@ -1,25 +1,73 @@
+import React, { useState } from "react";
+import axios from "axios"
+
 const Contact = (props: { lang: "pl" | "en" }) => {
+
+    const [responseMessage, setresponseMessage] = useState("");
+    const [email, setEmail] = useState("");
+    const [name, setName] = useState("");
+    const [message, setMessage] = useState("")
+
+
+    const submitForm = (e: React.FormEvent<HTMLButtonElement>) => {
+        e.preventDefault();
+        axios.post("/", {
+            email: email,
+            name: name,
+            message: message
+        }).then(response => {
+            if (response.status === 200) {
+                if (props.lang === "pl") {
+                    setresponseMessage("Pomyślnie wysłano wiadomość");
+                } else {
+                    setresponseMessage("Message sent successfully");
+                }
+            }
+        }).catch(e => {
+            if (props.lang === "pl") {
+                setresponseMessage("Wystąpił błąd podczas wykonywania żądania");
+            } else {
+                setresponseMessage("An error occured during the request");
+            }
+        })
+    }
+
     return <div id='contact' className="mb-4">
         <div className="fs-2 text-dark  text-center"> {props.lang === "pl" ? "Kontakt" : "Contact"}</div>
         <form action="/" method="post" className="col-10 col-sm-8 col-md-6 col-lg-4 mx-auto"  >
             <div className="mb-3">
                 <label htmlFor="name" className="form-label"> {props.lang === "pl" ? "Imię" : "Name"} </label>
-                <input type="text" name="name" className="form-control" id="name" />
+                <input
+                    value={name}
+                    onChange={(e) => { setName(e.target.value) }}
+                    type="text" name="name" className="form-control" id="name" />
             </div>
             <div className="mb-3">
-                <label htmlFor="Email" className="form-label">Email address</label>
-                <input type="email" className="form-control" id="email" />
+                <label htmlFor="Email" className="form-label"> {props.lang === "pl" ? "Adress email" : "Email address"} </label>
+                <input
+                    value={email}
+                    onChange={(e) => { setEmail(e.target.value) }}
+                    type="email" className="form-control" id="email" />
             </div>
             <div className="mb-3">
                 <label htmlFor="message" className="form-label"> {props.lang === "pl" ? "Wiadomość" : "Message"}</label>
-                <textarea defaultValue="" className="form-control" id="message" />
+                <textarea value={message}
+                    onChange={(e) => { setMessage(e.target.value) }}
+                    className="form-control" id="message" />
             </div>
 
 
             <div className="justify-content-center d-flex flex-row">
-                <button type="submit" className="btn customBorder text-lighterBlue ">
+                <button type="submit" onClick={submitForm} className="btn customBorder text-lighterBlue ">
                     {props.lang === "pl" ? "Wyślij" : "Send"}
                 </button>
+            </div>
+            <div className="text-center text-lighterBlue mt-3">
+                {/* Your request was successful */}
+                {responseMessage} {responseMessage !== "" ? <button className="btn customBorder text-dark"
+                onClick={e=>setresponseMessage("")}>
+                        X
+                </button> : ""}
             </div>
         </form>
     </div>
